12cf6f95dce4841a7d91359e6a4cb18511fba8bc
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:10:34 -0500

    Se realizo el ejercicio numero siete

---
+++ ejercicio_7.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Ingrese un numero: "))
+
+numero_duplicado = numero * numero;
+
+numero_triplicado = numero * numero * numero;
+
+alert("El doble del numero es de: "+ numero_duplicado)
+alert("El triple del numero es de: " + numero_triplicado)
