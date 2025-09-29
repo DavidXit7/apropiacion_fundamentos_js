09cd023cc43313226d0c1bdd165badf5c9beb0cc
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:09:30 -0500

    Se realizo el ejercicio numero tres

---
+++ ejercicio_3.js
@@ -0,0 +1,7 @@
+let nota_1 = parseFloat(prompt("Ingrese el valor de la primer nota: "));
+let nota_2 = parseFloat(prompt("Ingrese el valor de la segunda nota: "));
+let nota_3 = parseFloat(prompt("Ingrese el valor de la tercer nota: "));
+
+promedio = (nota_1 + nota_2 + nota_3) / 3;
+
+alert("El promedio de las notas es de:  "+promedio);
