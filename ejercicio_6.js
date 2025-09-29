3f6fdda1c37347866b2020a0658384bf5875b5b7
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:10:14 -0500

    Se realizo el ejercicio numero seis

---
+++ ejercicio_6.js
@@ -0,0 +1,5 @@
+let longitud_lado = parseFloat(prompt("Ingrese la longitud del lado del cuadrado a calcular: "))
+
+calcular_perimetro = 4 * longitud_lado
+
+alert("El perimetro del cuadrado es de: " + calcular_perimetro)
