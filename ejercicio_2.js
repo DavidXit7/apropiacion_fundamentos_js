1702712c9753cafeca3b257ec3c02ada139d973f
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:09:11 -0500

    Se realizo el ejercicio numero dos

---
+++ ejercicio_2.js
@@ -0,0 +1,6 @@
+let base = parseFloat(prompt("Digite el valor de la base: "));
+let altura = parseFloat(prompt("Digite el valor de la altura"));
+
+area = base * altura;
+
+alert("El area del rectangulo es de: "+ area);
