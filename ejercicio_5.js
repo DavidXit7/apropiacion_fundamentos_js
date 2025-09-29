598349d54dfc2b667c03e3174d68353c9fd552a9
David Rueda <Rueda9673@gmail.com>
2025-09-29 17:10:01 -0500

    Se realizo el ejercicio numero cinco

---
+++ ejercicio_5.js
@@ -0,0 +1,5 @@
+let temperatura = parseInt(prompt("Ingrese los grados celsius a convertir: "));
+
+conversion_fahrenheit = (temperatura * 9/5) + 32;
+
+alert("La conversion a Fahrenheit es de: " +conversion_fahrenheit)
